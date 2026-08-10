2a92fe64a8733680d344ea24ae2f9823e6148dcf
VardaAmin <wardaamin895@gmail.com>
2026-08-11 03:53:42 +0500

    Implement I-type instruction decoding

--- src/decoder.c
+++ src/decoder.c
@@ -1,5 +1,17 @@
 #include "decoder.h"
 
+static int32_t sign_extend(uint32_t value, unsigned int bits)
+{
+    uint32_t sign_bit = 1U << (bits - 1U);
+
+    if (value & sign_bit)
+    {
+        value |= ~((1U << bits) - 1U);
+    }
+
+    return (int32_t)value;
+}
+
 InstructionFields decode_fields(uint32_t instruction)
 {
     InstructionFields fields;
@@ -11,8 +23,10 @@ InstructionFields decode_fields(uint32_t instruction)
     fields.rs2    = EXTRACT_BITS(instruction, 24, 20);
     fields.funct7 = EXTRACT_BITS(instruction, 31, 25);
 
-    fields.immediate = 0;
-
+    fields.immediate = sign_extend(
+    EXTRACT_BITS(instruction, 31, 20),
+    12
+);
     return fields;
 }
 
@@ -96,6 +110,54 @@ InstructionType decode_instruction(const InstructionFields *fields)
                     }
 
                     break;
+
+                default:
+                    break;
+            }
+
+            break;
+
+        case 0x13:
+            switch (fields->funct3)
+            {
+                case 0x0:
+                    return INST_ADDI;
+
+                case 0x1:
+                    if (fields->funct7 == 0x00)
+                    {
+                        return INST_SLLI;
+                    }
+
+                    break;
+
+                case 0x2:
+                    return INST_SLTI;
+
+                case 0x3:
+                    return INST_SLTIU;
+
+                case 0x4:
+                    return INST_XORI;
+
+                case 0x5:
+                    if (fields->funct7 == 0x00)
+                    {
+                        return INST_SRLI;
+                    }
+
+                    if (fields->funct7 == 0x20)
+                    {
+                        return INST_SRAI;
+                    }
+
+                    break;
+
+                case 0x6:
+                    return INST_ORI;
+
+                case 0x7:
+                    return INST_ANDI;
 
                 default:
                     break;
